dba1b5cc02b778d8425a477f67a7552cb637a1ee
DanTeSss7 <dan2005gdi@gmail.com>
2023-10-17 20:25:14 +0300

    aall

--- Gres_IDB-22-12/Assets/New_script.cs
+++ Gres_IDB-22-12/Assets/New_script.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class New_script : MonoBehaviour
 {
@@ -18,5 +20,24 @@ public class New_script : MonoBehaviour
     {
         rend.material.color = newColor;
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+public class Counter : MonoBehaviour
+{
+    public int counterValue = 10; // Изначальное значение счетчика
+    public Text counterText; // Ссылка на компонент Text, на котором будет отображаться значение счетчика
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("OtherObject"))
+        {
+            counterValue--; // Уменьшаем значение счетчика при столкновении
+            UpdateCounterText(); // Обновляем отображение текста счетчика
+        }
+    }
+
+    private void UpdateCounterText()
+    {
+        counterText.text = "Counter: " + counterValue.ToString(); // Обновляем текст счетчика
     }
 }
